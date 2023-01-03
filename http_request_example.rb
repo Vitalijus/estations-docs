6081f6887bb1e523f5b4aed1e1ffc4b6aea9fe41
vitaly <vitalij.desuk@gmail.com>
2023-01-03 18:38:39 +0200

    Update http_request_example.rb

--- http_request_example.rb
+++ http_request_example.rb
@@ -23,24 +23,3 @@ query = <<~GQL
 
 response = HTTP.post(base_address, params: { query: query })
 p response.parse
-
-# Success response
-# {
-#   "data": {
-#     "userLogin": {
-#       "authenticatable": {
-#         "email": "dev@estations.io"
-#       },
-#       "credentials": {
-#         "accessToken": "OvZRVbA011HisJRjCAcNhA",
-#         "client": "q6ZRz4ib0LlMZWT-KbTJJg",
-#         "expiry": 1672763368,
-#         "tokenType": "Bearer",
-#         "uid": "dev@estations.io"
-#       }
-#     }
-#   }
-# }
-
-# Error response
-# {"data"=>{"userLogin"=>nil}, "errors"=>[{"message"=>"Invalid login credentials. Please try again.", "locations"=>[{"line"=>2, "column"=>3}], "path"=>["userLogin"], "extensions"=>{"code"=>"USER_ERROR"}}]}
