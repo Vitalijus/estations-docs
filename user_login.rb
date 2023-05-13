dc916bb3125319a0b28c29fa0e15ddea1db86bea
Vitalijus Desukas <vitalij.desuk@gmail.com>
2023-05-13 13:17:37 +0300

    Update user_login.rb

--- user_login.rb
+++ user_login.rb
@@ -3,23 +3,23 @@ require "http"
 base_address = "https://www.estations.io/graphql"
 
 login_mutation = <<~GQL
-mutation userLogin {
-  userLogin(
-    email: "dev@estations.io"
-    password: "password"
-  ){
-    authenticatable{
-      email
-    }
-    credentials{
-      accessToken
-      client
-      expiry
-      tokenType
-      uid
+  mutation userLogin {
+    userLogin(
+      email: "dev@estations.io"
+      password: "password"
+    ){
+      authenticatable{
+        email
+      }
+      credentials{
+        accessToken
+        client
+        expiry
+        tokenType
+        uid
+      }
     }
   }
-}
 GQL
 
 login_response = HTTP.post(base_address, params: { query: login_mutation })
